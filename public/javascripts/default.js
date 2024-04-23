98d4c036d0e051e20688c4db6de36fdf0de9fc6b
ZZ3n <ndsrhkd@naver.com>
2024-04-23 10:00:49 +0900

    for push gh

--- public/javascripts/default.js
+++ public/javascripts/default.js
@@ -7,7 +7,7 @@ const PASS = "pass"
 const VALID_LENGTH_MAX = 20;
 const VALID_LENGTH_MIN = 8;
 
-
+// 딤 처리
 function dimmed(flag = true) {
     const dimmedElem = document.getElementById("dimmed");
     if (flag) {
@@ -17,6 +17,7 @@ function dimmed(flag = true) {
     }
 }
 
+// 모달 켜기
 function modalOn(modalId) {
     let modalElem = document.getElementById(modalId);
     modalElem.style.display = "flex";
@@ -24,6 +25,7 @@ function modalOn(modalId) {
     document.body.style.overflow = "hidden";
 }
 
+// 모달 끄기
 function modalOff() {
     let modalList = document.getElementsByClassName("modal");
     for (let item of modalList) {
@@ -35,6 +37,7 @@ function modalOff() {
     document.body.style.overflow = "visible";
 }
 
+// 닉네임 validate, validateDuplicate 하면 중복검사까지 함.
 async function validateNickname(nickname, validateDuplicate = false) {
     const W_NICK_EMPTY = "*닉네임을 입력해주세요.";
     const W_NICK_LONG = "*닉네임은 최대 10자 까지 작성 가능합니다.";
@@ -68,6 +71,7 @@ async function validateNickname(nickname, validateDuplicate = false) {
     return PASS;
 }
 
+// 비밀번호 validate
 function validatePassword(userPassword, validateDuplicate = false) {
     const W_PWD_EMPTY = "*비밀번호를 입력해 주세요.";
     const W_PWD_FORMAT = "*비밀번호는 8자 이상, 20자 이하이며, 대문자, 소문자, 숫자, 특수문자를 각각 최소 1개 포함해야 합니다.";
@@ -84,6 +88,7 @@ function validatePassword(userPassword, validateDuplicate = false) {
     return PASS;
 }
 
+// 비밀번호 확인 validation
 function validatePasswordConfirmation(userPassword, userPasswordConfirmation) {
     if (userPasswordConfirmation.trim().length === 0) {
         return "*비밀번호를 한번 더 입력해주세요.";
@@ -95,6 +100,7 @@ function validatePasswordConfirmation(userPassword, userPasswordConfirmation) {
 }
 
 
+// 이메일 validation
 async function validateEmail(userEmail, validateDuplicate = false) {
     const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     const W_EMAIL_INVALID = "*올바른 이메일 주소 형식을 입력해 주세요. (예: example@example.com)";
@@ -131,15 +137,18 @@ async function validateEmail(userEmail, validateDuplicate = false) {
     return PASS;
 }
 
+// 헬퍼 텍스트 켜기
 function enableHelper(helperNode, text) {
     helperNode.style.display = "inline";
     helperNode.innerHTML = text;
 }
 
+// 헬퍼 텍스트 끄기
 function disableHelper(helperNode) {
     helperNode.style.display = "none";
 }
 
+// 파일 인풋 시 이미지 변경
 function displayImageOnChange(inputImage, displayImage) {
     const file = inputImage.files[0]
     const reader = new FileReader();
@@ -153,6 +162,7 @@ function displayImageOnChange(inputImage, displayImage) {
     }
 }
 
+// JSON파일을 object로 가져오기
 async function getJSON(path) {
     if (!path.endsWith(".json")) {
         path = path + ".json"
@@ -170,10 +180,12 @@ async function getJSON(path) {
         });
 }
 
+// 날짜 포맷팅
 function formatDateTime(date) {
     return `${date.getFullYear()}-${date.getMonth()}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
 }
 
+// html string -> dom element로 변환
 function fromHTML(html, trim = true) {
     html = trim ? html.trim() : html;
     if (!html) return null;
